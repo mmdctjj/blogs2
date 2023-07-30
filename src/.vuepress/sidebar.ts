752a593ec589e38f0321ede22c3ac8a18cd9efb5
mmdctjj <jiangpengjie@bizseer.com>
2023-07-30 20:10:38 +0800

    feat: 🚀 修改侧边栏

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,39 +1,9 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
-  "/": [
-    "",
-    {
-      text: "React系列",
-      icon: "react",
-      prefix: "react/",
-      children: "structure",
-    },
-    {
-      text: "Vue系列",
-      icon: "vue",
-      prefix: "vue/",
-      children: "structure",
-    },
-    {
-      text: "Vite系列",
-      icon: "tool",
-      prefix: "vite/",
-      children: "structure",
-    },
-    {
-      text: "新框架尝鲜系列",
-      icon: "geometry",
-      prefix: "framework/",
-      children: "structure",
-    },
-    {
-      text: "杂谈",
-      icon: "study",
-      prefix: "posts/",
-      children: "structure",
-    },
-    // "intro",
-    // "slides",
-  ],
+  "/react/":  "structure",
+  "/framework/":  "structure",
+  "/vite/":  "structure",
+  "/vue/":  "structure",
+  "/posts/":  "structure",
 });
